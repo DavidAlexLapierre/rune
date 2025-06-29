7eaab0dad35882ccfea8380544d123ce05c6a653
David-Alexandre Lapierre <davidalexlapierre@gmail.com>
2025-06-29 11:25:45 -0400

    made sure to keep LF files for .sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,8 @@
 # Get the directory this script is in
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+echo $SCRIPT_DIR
+
 # Read version from file
 VERSION=$(<"$SCRIPT_DIR/version.txt")
 
